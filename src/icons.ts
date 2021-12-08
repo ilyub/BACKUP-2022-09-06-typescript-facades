0d35e2c7b0ebce5e16d4a8f213bb5582e87ee1cb
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-08 11:10:32 +0300

    feat(icons,lang): Removed "createExcerpt" method

--- src/icons.ts
+++ src/icons.ts
@@ -1,8 +1,4 @@
-import * as cast from "@skylib/functions/dist/converters";
-import * as is from "@skylib/functions/dist/guards";
-import { createFacade, wrapProxyHandler } from "@skylib/functions/dist/helpers";
-import * as reflect from "@skylib/functions/dist/reflect";
-import type { ReadonlyRecord } from "@skylib/functions/dist/types/core";
+import { createFacade } from "@skylib/functions/dist/helpers";
 
 declare global {
   namespace facades {
@@ -13,47 +9,7 @@ declare global {
   }
 }
 
-export const icons = createFacade<Facade, Extension>("icons", {
-  createExcerpt<T extends Icon>(
-    keys: readonly T[],
-    dev: boolean
-  ): ReadonlyRecord<T, string> {
-    const keysSet = new Set<PropertyKey>(keys);
-
-    return dev
-      ? new Proxy(
-          icons,
-          wrapProxyHandler("icons.createExcerpt", {
-            get(target, key) {
-              if (keysSet.has(key)) return reflect.get(target, key);
-
-              if (is.string(key) && key.startsWith("__"))
-                return reflect.get(target, key);
-
-              throw new Error(`Unknown key: ${cast.string(key)}`);
-            },
-            getOwnPropertyDescriptor(target, key) {
-              return Object.getOwnPropertyDescriptor(target, key);
-            }
-          })
-        )
-      : icons;
-  }
-});
-
-export interface Extension {
-  /**
-   * Creates facade excerpt.
-   *
-   * @param keys - Keys.
-   * @param dev - Development mode.
-   * @returns Facade excerpt.
-   */
-  readonly createExcerpt: <T extends Icon>(
-    keys: readonly T[],
-    dev: boolean
-  ) => ReadonlyRecord<T, string>;
-}
+export const icons = createFacade<Facade, object>("icons", {});
 
 export type Facade = facades.icons.Facade;
 
