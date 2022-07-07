864ed095dacd31420f74b9d2d1008c1d8ea673f1
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-07-07 23:07:22 +0300

    feat(eslint): Edited rules

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,13 @@
 import * as facades from "@";
 import { o } from "@skylib/functions";
 
+interface Subtest {
+  readonly facade: object;
+  readonly name: string;
+}
+
 test.each(
-  // eslint-disable-next-line @skylib/custom/no-anonymous-return -- Ok
-  o.entries<string, object>(facades).map(([name, facade]) => {
-    return { facade, name };
-  })
+  o.entries(facades).map(([name, facade]): Subtest => ({ facade, name }))
 )("facades", ({ facade, name }) => {
   expect(() => o.get(facade, "unknown-property")).toThrow(
     new Error(`Missing facade implementation: ${name}`)
